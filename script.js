c124f975943fc1d7d2de38b4f1be13d50a183091
PMorehead <153697968+PMorehead@users.noreply.github.com>
2024-02-09 14:23:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -20,29 +20,13 @@ class MovieFinder extends React.Component {
         let { searchTerm } = this.state;
         searchTerm = searchTerm.trim();
         if (!searchTerm) {
-            return;
+          return;
         }
+      }
 
-        fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=5f3dbf65`)
-        .then(checkStatus)
-        .then(json)
-        .then(data => {
-            if (data.Response === 'False') {
-            throw new Error(data.Error);
-            }
-            if (data.Response === 'True' && data.Search) {
-            this.setState({ results: data.Search, error: '' });
-            }
-        })
-        .catch(error => {
-            this.setState({ error: error.message });
-            console.log(error);
-        })
-    }
+    render() {
+        const { searchTerm, results} = this.state;
 
-    render () {
-        const { searchTerm, results } = this.state;
-        
         return (
             <div className="container">
                 <div className="row">
@@ -58,7 +42,7 @@ class MovieFinder extends React.Component {
                             return results.map((movie) => {
                                 return <Movie key={movie.imdbID} movie={movie} />
                             })
-                        })}
+                        })()}
                     </div>
                 </div>
             </div>
@@ -72,12 +56,12 @@ const Movie = (props) => {
     return (
         <div className="row">
             <div className="col-4 col-md-3 mb-3">
-                <a href={`https://imdb.com/title/${imdbID}/`} target="_blank">
+                <a href={`https://www.imbd.com/title/${imdbID}/`} target="_blank">
                     <img src={Poster} className="img-fluid" />
                 </a>
             </div>
-            <div className="col-4 col-md-3 mb-3">
-                <a href={`https://imdb.com/title/${imdbID}/`} target="_blank">
+            <div className="col-8 col-md-9 mb-3">
+                <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
                     <h4>{Title}</h4>
                     <p>{Type} | {Year}</p>
                 </a>
@@ -88,14 +72,31 @@ const Movie = (props) => {
 
 const checkStatus = (response) => {
     if (response.ok) {
-        return response;
+      // .ok returns true if response status is 200-299
+      return response;
     }
-
-    throw new Error('Request was either a 404 or 500')
-}
-
-const json = (response) => response.json();
+    throw new Error('Request was either a 404 or 500');
+  }
+  
+  const json = (response) => response.json()
+  
+  fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=5f3dbf65`)
+    .then(checkStatus)
+    .then(json)
+    .then(data => {
+      if (data.Response === 'False') {
+        throw new Error(data.Error);
+      }
+  
+      if (data.Response === 'True' && data.Search) {
+        this.setState({ results: data.Search, error: '' });
+      }
+    })
+    .catch(error => {
+      this.setState({ error: error.message });
+      console.log(error);
+    })
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<MovieFinder />);
+root.render(<MovieFinder />)
